4d90b52221a0b18b48a29ef618e67250f082289b
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-11 22:14:32 +0100

    add style to form and title, create api service with axios to pokemon api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import pokeLogo from '../../assets/pokemon.png';
+
+import api from '../../services/api';
+
+import { Title, Form } from './styles';
+
+const Dashboard: React.FC = () => {
+  const [newPoke, setNewPoke] = useState('');
+
+  async function handleAddPokemon(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    try {
+      const response = await api.get(`pokemon/${newPoke}`);
+
+      const pokemon = response.data;
+
+      console.log('Pokemon: ', pokemon);
+    } catch (err) {
+      console.log('Apanhei um erro: ', err);
+    }
+  }
+  return (
+    <>
+      <img src={pokeLogo} width="150px" alt="Pokemon Explorer" />
+      <Title>Explore Pokémons como no Pokedex </Title>
+      <Form onSubmit={handleAddPokemon}>
+        <input
+          value={newPoke}
+          onChange={e => setNewPoke(e.target.value)}
+          placeholder="Digite o nome do pokémon"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
+};
 
 export default Dashboard;
