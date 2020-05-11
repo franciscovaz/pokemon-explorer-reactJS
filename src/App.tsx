8b547710ac3607117e2c1d55c5d40bec10452323
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-11 21:36:26 +0100

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <>
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
+    <GlobalStyle />
   </>
 );
 
